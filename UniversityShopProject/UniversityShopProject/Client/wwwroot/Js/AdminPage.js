089d0b4ed44870631d48860d09943121c4eb3a8f
Hosein <hoseinb2.0181@gmail.com>
2023-06-02 00:16:05 +0430

    day 2 Compelete

---
+++ UniversityShopProject/UniversityShopProject/Client/wwwroot/Js/AdminPage.js
@@ -0,0 +1,12 @@
+function CloseNav()
+{
+    document.getElementById("navbar").style.width = "0px";
+    document.getElementById("main").style.marginRight = "3%";
+    document.getElementById("openIcon").style.visibility = "visible";
+}
+function OpenNav()
+{
+    document.getElementById("navbar").style.width = "18%";
+    document.getElementById("main").style.marginRight = "20%";
+    document.getElementById("openIcon").style.visibility = "hidden";
+}
