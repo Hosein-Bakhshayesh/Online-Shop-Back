129c2364748d1a9979ded7154a03c8e9349eed6b
Hosein <hoseinb2.0181@gmail.com>
2023-06-03 01:40:59 +0330

    Compelete Day 2 ,Create Admin Panel

---
+++ UniversityShopProject/UniversityShopProject/Server/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniversityShopProjectModels.Context;
+using UniversityShopProjectModels.Models;
+using UniversityShopProjectServices.Service;
+
+namespace UniversityShopProject.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UniversityShopProjectContext db = new UniversityShopProjectContext();
+        UserService _userService;
+        public UserController()
+        {
+            _userService = new UserService(db);
+        }
+        [Route("List")]
+        public  ActionResult UserList()
+        {
+            List<User> users = _userService.GetAll().ToList();
+            if(users != null)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return NotFound("User Not Exist");
+            }
+        }
+    }
+}
